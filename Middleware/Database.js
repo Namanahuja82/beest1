2993f76b25861c8a4c3d90f9575b1830c53be2c4
Naman Ahuja <96652306+Namanahuja82@users.noreply.github.com>
2024-11-30 10:24:46 +0530

    Update Database.js

--- Middleware/Database.js
+++ Middleware/Database.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
 
-mongoose.connect("mongodb+srv://naman0908be21:naman@cluster0.peazls7.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("")
 .then(()=>console.log("Database Connected Successfully"))
 .catch((err)=>console.log(err));
